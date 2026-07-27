c0726e56137c82ac00b9214d4cf0c3292faf9a66
Daniel Willett - Mint Laptop <willettdr1@gmail.com>
2026-07-27 22:54:06 +0100

    Day 1 part 1

--- python/2025/day1.py
+++ python/2025/day1.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+file = open("day1-input")
+actions = []
+for line in file:
+    actions.append(line)
+file.close()
+
+#We can use actions[i][0] for L/R
+#And use actions[i][1:] for the value that comes after
+counter = 0
+position = 50
+for action in actions:
+    action = action.rstrip('\n')
+    if action[0] == "L":
+        position -= int(action[1:])
+    if action[0] == "R":
+        position += int(action[1:])
+    position = position % 100
+    if position == 0:
+        counter += 1
+
+print(counter)
